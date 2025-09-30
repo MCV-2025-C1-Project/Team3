aed77db6e96fc7039ada54426a33c7fe437d9ceb
GerardAsbert <114249151+GerardAsbert@users.noreply.github.com>
2025-09-30 20:59:32 +0200

    X2 distance

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -31,6 +31,26 @@ class Metrics:
 
         """
         return np.sum(np.abs(x - y))
+
+    
+    @staticmethod
+    def x2_dist(hist1: NDArray[np.float64], hist2: NDArray[np.float64]) -> float:
+    """
+    Input:
+    -hist1 (1-D numpy array): First image descriptor
+    -hist2 (1-D numpy array): Second image descriptor
+    
+    Returns:
+    -total (float): X2 distance between the two descriptors
+    """
+
+    numerator = (hist1 - hist2) ** 2
+    denominator = hist1 + hist2
+
+    #For avoiding division by zero
+    denominator[denominator == 0] = 1e-10
+
+    return np.sum(numerator / denominator)
     
 metrics = Metrics()
 print(metrics.l1_distance(np.array([1,2,3,4]),np.array([2,2,2,2])))
